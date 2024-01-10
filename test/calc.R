5f246ca4c9993f5f94740f218c804cf41c346579
sergioventurini <sergio.venturini@gmail.com>
2024-01-10 15:57:54 +0100

    Update test.R file
    Test.R file updated using the Rcpp implementation

--- test/calc.R
+++ test/calc.R
@@ -2,14 +2,14 @@ t <- 1
 n <- 7
 m <- 10
 # debugonce(range_probability)
-print(range_probability(t, n, m), digits = 15)
+print(range_probability_C(t, n, m), digits = 15)
 
 ###
 
 t <- 2
 n <- 70
 m <- 30
-print(smallest_order_value(t, n, m), digits = 15)
+print(smallest_order_value_C(t, n, m), digits = 15)
 
 ###
 
@@ -18,24 +18,24 @@ n <- 60
 m <- 30
 J <- 3
 # debugonce(highest_order_statistics)
-print(highest_order_statistics(t, n, m, J), digits = 15)
+print(highest_order_statistics_C(t, n, m, J), digits = 15)
 
 ###
 
 t <- 2
 n <- 6
 m <- 3
-print(max_order_statistic(t, n, m), digits = 15)
+print(max_order_statistic_C(t, n, m), digits = 15)
 
 t <- 2
 n <- 6
 m <- 7
-print(max_order_statistic(t, n, m), digits = 15)
+print(max_order_statistic_C(t, n, m), digits = 15)
 
 t <- 32
 n <- 60
 m <- 3
-print(max_order_statistic(t, n, m), digits = 15)
+print(max_order_statistic_C(t, n, m), digits = 15)
 
 ###
 
@@ -45,7 +45,7 @@ n <- 10
 m <- 5
 
 for (t in 2:16) {
-  res[t - 1] <- max_order_statistic(t, n, m)
+  res[t - 1] <- max_order_statistic_C(t, n, m)
 }
 res
 
@@ -56,7 +56,7 @@ res <- numeric(19)
 J <- 2
 
 for (t in 5:23) {
-  res[t - 4] <- highest_order_statistics(t, n, m, J)
+  res[t - 4] <- highest_order_statistics_C(t, n, m, J)
 }
 res
 
@@ -69,17 +69,17 @@ m <- 15
 J <- 3
 
 for (t in 8:34) {
-  res[t - 7] <- highest_order_statistics(t, n, m, J)
+  res[t - 7] <- highest_order_statistics_C(t, n, m, J)
 }
 res
 
-highest_order_statistics(8, 15, 15, 3)
+highest_order_statistics_C(8, 15, 15, 3)
 
 ###
 
 res <- numeric(19)
 
 for (t in 5:23) {
-    res[t - 4] <- smallest_order_value(t, n, m)
+    res[t - 4] <- smallest_order_value_C(t, n, m)
 }
 res
