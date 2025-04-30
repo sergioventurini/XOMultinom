d47d7b43ac93fba2f9bba5f9bbd37a5c1620d61b
sergioventurini <sergio.venturini@gmail.com>
2025-04-30 14:14:29 +0200

    small change
    small change

--- test/comparison.R
+++ test/comparison.R
@@ -16,3 +16,20 @@ system.time(pmaxmultinom(x = 25, size = n, prob = probs, log = FALSE,
 
 system.time(pmaxmultinom(x = 25, size = n, prob = probs, log = FALSE,
                          verbose = FALSE, method = "Corrado"))
+
+###
+
+k <- 10
+nvals <- c(100, 150, 200, 300, 500)
+times <- numeric(length(nvals))
+set.seed(101)
+# probs <- rdirichlet(1, rep(1/k, k))
+probs <- rdirichlet(1, rep(1, k))
+# probs <- rep(1/k, k)
+for (i in seq_along(nvals)) {
+  cat(paste0("n value = ", nvals[i], "\n"))
+  xvals <- 0:nvals[i]
+  times[i] <- system.time(pmaxmultinom(x = xvals, size = nvals[i], prob = probs, log = FALSE,
+                                       verbose = FALSE, method = "Corrado"))[3]
+}
+times
